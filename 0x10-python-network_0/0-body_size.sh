c455d1086c0328b88a72815566710406a1f7336d
root <gilbertoforiboye@gmail.com>
2024-03-30 08:06:56 -0700

    kkl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a request to that URL using curl, and displays the size of the response body in bytes
+# This script takes a URL as an argument, sends a request to that URL using curl in silent mode, and displays the size of the response body in bytes
 
-curl -s -w "%{size_download}" -o /tmp/response_body "$1"
+curl -s -w "%{size_download}" -o /dev/null "$1"
 
