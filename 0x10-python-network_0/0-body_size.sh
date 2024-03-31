4d5060121943ece5f3c3820b3ce41a61da511551
Gilbert Ofori-Boye <gilbertoforiboye@gmail.com>
2024-03-31 00:05:04 +0000

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/0-body_size.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This script takes a URL as an argument, sends a request to that URL using curl in silent mode, and displays the size of the response body in bytes
-
-curl -s -w "%{size_download}" -o /dev/null "$1"
-
